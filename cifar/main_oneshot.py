dbcf9eeed299033f7332129165eab60ec676c553
Bo Chen <bochen1993cs@gmai.com>
2022-11-12 09:51:55 -0600

    test

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -677,11 +677,13 @@ def gen_partition_mask(net_id,mask_size):
     r = args.partition_ratio
     # 1st accurate
     if net_id == 0:
-        mask[:int(c1*(1-r)),:int(c2*(1-r))] = 1
-        mask[int(c1*(1-r)):,int(c2*(1-r)):] = 1
+        if c1 == c2:
+            mask[:int(c1*(1-r)),:int(c2*(1-r))] = 1
+            mask[int(c1*(1-r)):,int(c2*(1-r)):] = 1
     elif net_id == 1:
-        mask[:int(c1*r),:int(c2*r)] = 1
-        mask[int(c1*r):,int(c2*r):] = 1
+        if c1 == c2:
+            mask[:int(c1*r),:int(c2*r)] = 1
+            mask[int(c1*r):,int(c2*r):] = 1
     # 2nd accurate
     elif net_id == 2:
         # upper part
@@ -691,7 +693,6 @@ def gen_partition_mask(net_id,mask_size):
         else:
             # first conv
             mask[:int(c1*(1-r))] = 1
-            exit(0)
     elif net_id == 3:
         # lower part
         # mask[int(c1*r):,int(c2*r):] = 1
@@ -699,7 +700,6 @@ def gen_partition_mask(net_id,mask_size):
             mask[int(c1*r):,int(c2*r):] = 1
         else:
             mask[int(c1*r):] = 1
-            exit(0)
     return mask.view(*mask_size,1,1)
 
 def sample_partition_network(old_model,net_id=None,eval=False):
@@ -717,7 +717,6 @@ def sample_partition_network(old_model,net_id=None,eval=False):
         with torch.no_grad():
             if isinstance(sub_module, nn.Conv2d): 
                 mask_size = (sub_module.weight.size(0),sub_module.weight.size(1))
-                if mask_size[0]<=3 or mask_size[1]<=3:continue
                 mask = gen_partition_mask(net_id,mask_size)
                 sub_module.weight.data *= mask
     return dynamic_model

aa84bd3e81c7dd23b375e038159e2bc9b4f8f743
Bo Chen <bochen1993cs@gmai.com>
2022-11-12 09:59:56 -0600

    test

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -677,17 +677,11 @@ def gen_partition_mask(net_id,mask_size):
     r = args.partition_ratio
     # 1st accurate
     if net_id == 0:
-        if c1 == c2:
-            mask[:int(c1*(1-r)),:int(c2*(1-r))] = 1
-            mask[int(c1*(1-r)):,int(c2*(1-r)):] = 1
-        else:
-            mask[:] = 1
+        mask[:int(c1*(1-r)),:int(c2*(1-r))] = 1
+        mask[int(c1*(1-r)):,int(c2*(1-r)):] = 1
     elif net_id == 1:
-        if c1 == c2:
-            mask[:int(c1*r),:int(c2*r)] = 1
-            mask[int(c1*r):,int(c2*r):] = 1
-        else:
-            mask[:] = 1
+        mask[:int(c1*r),:int(c2*r)] = 1
+        mask[int(c1*r):,int(c2*r):] = 1
     # 2nd accurate
     elif net_id == 2:
         # upper part
@@ -721,9 +715,12 @@ def sample_partition_network(old_model,net_id=None,eval=False):
         with torch.no_grad():
             if isinstance(sub_module, nn.Conv2d): 
                 mask_size = (sub_module.weight.size(0),sub_module.weight.size(1))
+                # prev = sub_module.weight.data.clone().detach()
                 if mask_size[0]<=3 or mask_size[1]<=3:continue
                 mask = gen_partition_mask(net_id,mask_size)
                 sub_module.weight.data *= mask
+                # if mask_size[0]<=3 or mask_size[1]<=3:
+                #     assert torch.equal(prev,sub_module.weight.data)
     return dynamic_model
 
 def update_partitioned_model(old_model,new_model,net_id,batch_idx):

ea86acba0ddd131bdaf496f728d7d73e7c00fa8b
Bo Chen <bochen1993cs@gmai.com>
2022-11-12 23:20:04 -0600

    test

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -390,6 +390,21 @@ if args.VLB_conv:
                                     nn.Conv2d(128, model.in_planes, kernel_size=3, stride=1, padding=1, bias=False),
                                     nn.BatchNorm2d(model.in_planes),
                                     nn.ReLU()).cuda()
+    elif args.VLB_conv_type == 3:
+        sampling_interval = 3
+        # better with at least 3 layers
+        model.aggr = nn.Sequential(nn.Conv2d(352, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(),
+                                    nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(),
+                                    nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(64),
+                                    nn.ReLU(),
+                                    nn.Conv2d(64, model.in_planes, kernel_size=3, stride=1, padding=1, bias=False),
+                                    nn.BatchNorm2d(model.in_planes),
+                                    nn.ReLU()).cuda()
     else:
         exit(0)
     from types import MethodType

28a863970ddb7e5b434b626217b5db4b34c3c032
Bo Chen <bochen1993cs@gmai.com>
2022-11-14 12:26:09 -0600

    type 9

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -481,13 +481,6 @@ if args.VLB_conv:
         # to try
         sampling_interval = 3
         # better with at least 3 layers
-        model.aggr = nn.Sequential(nn.Conv2d(352, model.in_planes, kernel_size=3, stride=1, padding=1, bias=False),
-                                    nn.BatchNorm2d(model.in_planes),
-                                    nn.ReLU()).cuda()
-    elif args.VLB_conv_type == 10:
-        # to try
-        sampling_interval = 3
-        # better with at least 3 layers
         model.aggr = nn.Sequential(nn.Conv2d(352, 192, kernel_size=3, stride=1, padding=1, bias=False),
                                     nn.BatchNorm2d(192),
                                     nn.ReLU(),

fbe1177bc143a9a051f4d200d179fdd0d5845d7d
Bo Chen <bochen1993cs@gmai.com>
2022-11-16 19:46:45 -0600

    add partition to imagenet

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -642,7 +642,7 @@ def main_worker(gpu, ngpus_per_node, args):
             x = self.fc(x)
 
             return x, None
-        model.forward = MethodType(modified_forward, model)
+        model.module.forward = MethodType(modified_forward, model)
 
     # optionally resume from a checkpoint
     if args.resume:

817736505c6355cdeefe90a0760bb4e761c44f2d
Bo Chen <bochen1993cs@gmai.com>
2022-11-23 17:20:39 -0600

    debug sim

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -435,6 +435,7 @@ if args.VLB_conv:
         out = F.relu(self.bn1(self.conv1(x)))
         out_list.append(F.avg_pool2d(out, 4))
         for idx,l in enumerate(self.layer1):
+            print(idx,out.size())
             out = l(out)
             if idx%sampling_interval == sampling_interval-1:
                 out_list.append(F.avg_pool2d(out, 4))

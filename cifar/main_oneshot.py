f571b0e7b194f8597d016c8c86cb39008de49bb7
Bo Chen <bochen1993cs@gmai.com>
2022-11-23 17:19:35 -0600

    debug sim

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -287,7 +287,6 @@ if args.cuda:
     model.cuda()
 
 BASEFLOPS = compute_conv_flops(model, cuda=True)
-print('Baseflops:',BASEFLOPS)
 
 if args.loss in {LossType.PROGRESSIVE_SHRINKING,LossType.PARTITION}:
     teacher_model = copy.deepcopy(model)
--- cifar/models/resnet_expand.py
+++ cifar/models/resnet_expand.py
@@ -193,7 +193,6 @@ class BasicBlock(BuildingBlock):
             out = self.gate2(out)
 
             out = self.expand_layer(out)
-            print(out.size())
             out += self.shortcut(x)
             out = F.relu(out)
             return out

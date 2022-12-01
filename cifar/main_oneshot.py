4e01a9bd85a95eabc0c695377c71f1abc6c24ff4
Bo Chen <bochen1993cs@gmai.com>
2022-11-30 22:29:27 -0600

    simulate 3 and 4 nodes

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -1280,16 +1280,12 @@ def analyze_all_recorded_traces():
         latency_list = []
         num_of_line = 0
         for row in csv_reader:
-            if float(row["latency"])<0 or float(row["downthrpt"])<100000:
+            if float(row["latency"])>1e7 or float(row["latency"])<0 or float(row["downthrpt"])>1e8:
                 print('Warning:',row)
                 continue
             for bs in [2**i for i in range(7)]:
                 query_size = 3*32*32*4*bs # bytes
                 latency_list += [query_size/float(row["downthrpt"]) + float(row["latency"])/1e6]
-                if latency_list[-1]>1000:
-                    print(latency_list[-1])
-                    print(row)
-                    exit(0)
             num_of_line += 1
         latency_list = np.array(latency_list).reshape((num_of_line,7))
         latency_mean,latency_std = latency_list.mean(axis=0),latency_list.std(axis=0)

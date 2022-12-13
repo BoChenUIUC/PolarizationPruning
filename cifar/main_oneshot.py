9fe8f671beaa02d2bbd1ff0ea41597a534e6f320
Bo Chen <bochen1993cs@gmai.com>
2022-12-13 01:00:13 -0600

    check gpu

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -1445,9 +1445,8 @@ def simulation(model, arch, prune_mode, num_classes):
         for i in range(len(args.alphas)):
             print(i,'be',torch.cuda.memory_allocated(0)/1024/1024)
             masked_model = sample_partition_network(model,net_id=i,inplace=False)
-            masked_model.cpu()
             print(i,'af',torch.cuda.memory_allocated(0)/1024/1024)
-            masked_model.cuda()
+            continue
             flop = compute_conv_flops_par(masked_model, cuda=True)
             all_flop_ratios += [flop/BASEFLOPS]
             map_time_lst,reduce_time_lst,correct_lst = test(masked_model,map_reduce=True)

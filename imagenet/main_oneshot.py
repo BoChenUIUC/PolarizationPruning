26dfe9d1df13b61437dd6287efc163f3a929991c
Bo Chen <bochen1993cs@gmai.com>
2022-12-16 18:35:04 -0600

    imagenet debug

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1195,6 +1195,7 @@ def sample_partition_network(args,old_model,net_id=None,deepcopy=True,inplace=Tr
                     if sub_module.weight.size(1) == 3:
                         sub_module.weight.data = sub_module.weight.data[out_chan_mask,:].clone()
                     elif sub_module.weight.size(1) == 1:
+                        print(sub_module.weight.size())
                         sub_module.weight.data = sub_module.weight.data[out_chan_mask,:].clone()
                         sub_module.groups = int((out_chan_mask).sum())
                     else:

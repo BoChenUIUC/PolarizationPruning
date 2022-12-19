115c3c047fb7034b1f08729c3d21b411d262c9e9
Bo Chen <bochen1993cs@gmai.com>
2022-12-19 14:16:30 -0600

    imagenet ch=256

--- imagenet/main_oneshot.py
+++ imagenet/main_oneshot.py
@@ -1534,6 +1534,7 @@ def partition_while_training(model, arch, prune_mode, width_multiplier, val_load
             masked_model = sample_partition_network(args,model,net_id=i)
             flop = compute_conv_flops_par(masked_model, cuda=True)
             print(flop/args.BASEFLOPS)
+            continue
             prec1 = validate(val_loader, masked_model, criterion, epoch=epoch, args=args, writer=None)
             saved_prec1s += [prec1]
             saved_flops += [flop]

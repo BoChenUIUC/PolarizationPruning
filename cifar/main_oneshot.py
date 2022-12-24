4bb0fdb88e09d6739063b5adac2d7d97fca11d8b
Bo Chen <bochen1993cs@gmai.com>
2022-12-24 11:15:40 -0600

    cifar eval nolab

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -1386,13 +1386,13 @@ def analyze_trace_metrics(metrics_of_all_traces,metrics_shape):
         latency_breakdown[0] += RMLaaS_latency_breakdown
         latency_breakdown[1] += no_rep_latency_breakdown
         latency_breakdown[2] += total_rep_latency_breakdown
-    # print('Accuracy and latency stats...')
-    # for stats in [all_accuracy,all_latency]:
-    #     # print(stats)
-    #     stats = np.array(stats)
-    #     print(stats.mean(axis=-1).tolist())
-    #     print(stats.std(axis=-1).tolist())
-    # print('Effective accuracy...')
+    print('Accuracy and latency stats...')
+    for stats in [all_accuracy,all_latency]:
+        # print(stats)
+        stats = np.array(stats)
+        print(stats.mean(axis=-1).tolist())
+        print(stats.std(axis=-1).tolist())
+    print('Effective accuracy...')
     for stats in [all_effective_accuracy]:
         stats = np.array(stats).reshape(metrics_shape)
         r2 = (stats[2]-stats[0]).max(axis=1)

bf4e7c863914d86168ab805463b1ee4caf92fda7
Bo Chen <bochen1993cs@gmai.com>
2023-01-05 10:12:56 -0600

    sim

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -1412,6 +1412,7 @@ def analyze_trace_metrics(metrics_of_all_traces,metrics_shape):
     print('Reliability...')
     for stats in [all_effective_accuracy]:
         stats = np.array(stats).reshape(metrics_shape)
+        print(stats)
         r2 = (stats[2]-stats[0]).max(axis=1)
         r3 = (stats[3]-stats[0]).max(axis=1)
         r4 = (stats[4]-stats[0]).max(axis=1)

a09313228b82063a65d3f8713135fa8a3db07cf6
Bo Chen <bochen1993cs@gmai.com>
2023-02-01 20:04:43 -0600

    test

--- cifar/main_oneshot.py
+++ cifar/main_oneshot.py
@@ -1446,10 +1446,10 @@ def analyze_trace_metrics(metrics_of_all_traces,metrics_shape):
         stats = np.array(stats).reshape(metrics_shape)
         # print(stats.mean(axis=1).tolist())
         # print(stats.std(axis=1).tolist())
-        # if stats.shape[-1] != args.num_ddls:
-        #     stats = stats[:,:,:5]
-        # else:
-        #     stats = stats[:,:,-5:]
+        if stats.shape[-1] != args.num_ddls:
+            stats = stats[:,:,:5]
+        else:
+            stats = stats[:,:,-5:]
         print((stats[[2]]-stats[[0]]).min(axis=1).max())
         print((stats[[2]]-stats[[0]]).mean(axis=1).max())
         print((stats[[2]]-stats[[0]]).max(axis=1).max())

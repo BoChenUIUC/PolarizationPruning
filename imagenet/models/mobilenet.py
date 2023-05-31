34dc958c63e98dc85c32c2aa1250398fdd8eb00e
Bo Chen <bochen1993cs@gmai.com>
2023-05-31 18:04:34 -0500

    test comp

--- imagenet/models/mobilenet.py
+++ imagenet/models/mobilenet.py
@@ -601,7 +601,10 @@ class MobileNetV2(nn.Module):
     def _forward_impl(self, x):
         # This exists since TorchScript doesn't support inheritance, so the superclass method
         # (this one) needs to have a name other than `forward` that can be accessed in a subclass
-        x = self.features(x)
+        # x = self.features(x)
+        for l in self.features:
+            x = l(x)
+            print(x.size())
         # Cannot use "squeeze" as batch-size can be 1 => must use reshape with x.shape[0]
         x = nn.functional.adaptive_avg_pool2d(x, 1).reshape(x.shape[0], -1)
         x = self.classifier(x)
